e37f6124a0ca7d9c78e10870c972b735fff6ad7f
Khoa Bui <thomasbui93@gmail.com>
2019-05-12 20:02:25 +0100

    add ips restrictions

--- analytics/views/create.py
+++ analytics/views/create.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from analytics.models.analytics import Analytics
 from analytics.services.ip import get_ip_from_request
+from analytics.services.data_validate import validate_ip
 from project.models.project import Project
 
 @csrf_exempt
@@ -19,6 +20,10 @@ def create_action(request):
     try:
         encoded_data = request.POST.get('data')
         ip = get_ip_from_request(request)
+        
+        if not validate_ip(ip, project_id):
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+        
         data = Analytics.objects.create(data = encoded_data, ip = ip, project_id = project_id)
         return HttpResponse(status=status.HTTP_201_CREATED)
     except BaseException:
--- project/models/project.py
+++ project/models/project.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     description = models.TextField()
+    ips = models.TextField(default='')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField('created date', auto_now_add=True)
     updated_date = models.DateTimeField('updated date', auto_now=True)
