5e7aa2017e4bcbd14eb2ed48ce99c96c7cc59f63
Khoa Bui <thomasbui93@gmail.com>
2019-05-14 18:30:52 +0100

    remove validate_data

--- analytics/services/data_validate.py
+++ analytics/services/data_validate.py
@@ -1,5 +1,6 @@
 from project.models.project import Project
 from django.shortcuts import get_object_or_404
+from analytics_schema.models.analytics_schema import AnalyticsSchema
 
 def validate_ip(ip, project_id):
     project = get_object_or_404(Project, id = project_id)
@@ -12,3 +13,7 @@ def validate_ip(ip, project_id):
     else:
         ips = project.ips.split(',')
         return ips.index(ip) > -1
+
+
+def validate_data(ip, project_id):
+    return validate_ip(ip, project_id)
